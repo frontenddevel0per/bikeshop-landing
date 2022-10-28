09eb324c0be06666738eaf45d0fad50363443469
Leonid <leonovozhilov1967@gmail.com>
2022-10-28 13:45:21 +0300

    style: changed sliders scrollbar

--- components/features/features.component.tsx
+++ components/features/features.component.tsx
@@ -10,30 +10,32 @@ const Features: FC = () => {
     <section className="features" id="features">
       <h2 className="section-title">Key features</h2>
       <div className="features-slider">
-        <FeaturesCard
-          src={galleryimg1}
-          alt="slider-img-1"
-          title="Removable battery"
-          subtitle="The battery goes the distance with you and clicks right back into place."
-        />
-        <FeaturesCard
-          src={galleryimg2}
-          alt="slider-img-2"
-          title="Assistance"
-          subtitle="A swift first kick powers the speed you need for an active ride and an easy climb."
-        />
-        <FeaturesCard
-          src={galleryimg3}
-          alt="slider-img-3"
-          title="Wireless charging"
-          subtitle="Once docked in the cockpit, your phone is in a state of charge."
-        />
-        <FeaturesCard
-          src={galleryimg2}
-          alt="slider-img-4"
-          title="Integrated lights"
-          subtitle="Lights integrated into the frame give you always on visibility day and night."
-        />
+        <div className="features-slider-cards">
+          <FeaturesCard
+            src={galleryimg1}
+            alt="slider-img-1"
+            title="Removable battery"
+            subtitle="The battery goes the distance with you and clicks right back into place."
+          />
+          <FeaturesCard
+            src={galleryimg2}
+            alt="slider-img-2"
+            title="Assistance"
+            subtitle="A swift first kick powers the speed you need for an active ride and an easy climb."
+          />
+          <FeaturesCard
+            src={galleryimg3}
+            alt="slider-img-3"
+            title="Wireless charging"
+            subtitle="Once docked in the cockpit, your phone is in a state of charge."
+          />
+          <FeaturesCard
+            src={galleryimg2}
+            alt="slider-img-4"
+            title="Integrated lights"
+            subtitle="Lights integrated into the frame give you always on visibility day and night."
+          />
+        </div>
       </div>
     </section>
   );
